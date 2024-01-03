eaf4d1977a2664f2b4fe8e3b22a110a28698633e
Piyush Joshi <122557996+piiyushJoshi@users.noreply.github.com>
2024-01-03 23:24:56 +0530

    Create Matrix Chain Multiplication.cpp

---
+++ Matrix Chain Multiplication.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int mcm(int p[], int n){
+	int m[n][n];
+	int i, j, k, l, q;
+	for (i = 1; i < n; i++)
+		m[i][i] = 0;
+
+	for (l = 2; l < n; l++)
+		for (i = 1; i < n - l + 1; i++){
+			j = i + l - 1;
+			m[i][j] = INT_MAX;
+			for (k = i; k <= j - 1; k++){
+				q = m[i][k] + m[k + 1][j]
+					+ p[i - 1] * p[k] * p[j];
+				if (q < m[i][j])
+					m[i][j] = q;
+			}
+		}
+	return m[1][n - 1];
+}
+
+int main(){
+	int arr[] = {5,4,6,2,7};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	cout << "Minimum number of multiplications required is: "<< mcm(arr, size);
+	return 0;
+}
